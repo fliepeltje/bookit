8562b056b818f0f52ff6a9c2fb83d93ba79da66d
Donatas Rasiukevicius <d.rasiukevicius@labdigital.nl>
2020-07-09 09:15:42 +0200

    Refactor time arg to use fromstr trait

--- src/book.rs
+++ src/book.rs
@@ -1,12 +1,51 @@
 use chrono::{NaiveDate, Weekday};
+use std::str::FromStr;
 use structopt::StructOpt;
 use time::Time;
 
-#[derive(Debug, PartialEq, Eq)]
-enum TimeLog {
+#[derive(Debug, PartialEq)]
+enum TimeArg {
     Minutes(u8),
-    Hours(u8),
+    Hours(f32),
     Stretch(Time, Time),
+}
+
+type TimeArgError = String;
+
+impl FromStr for TimeArg {
+    type Err = TimeArgError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.contains("::") {
+            match (s.get(..1), s.get(3..)) {
+                (Some("h"), Some(hours)) => match hours.parse::<f32>() {
+                    Ok(h) => Ok(Self::Hours(h)),
+                    Err(ctx) => Err(format!("{}", ctx)),
+                },
+                (Some(s), Some(stretch)) if s == "s" || s == "t" => {
+                    if stretch == "last" {
+                        Ok(Self::Stretch(Time::now(), Time::now()))
+                    } else {
+                        let time = Time::parse(format!("{}:00", stretch), "%T")
+                            .expect("incorrect time argument (HH:MM or last)");
+                        match s {
+                            "s" => Ok(Self::Stretch(time, Time::now())),
+                            "t" => Ok(Self::Stretch(Time::now(), time)),
+                            _ => Err("unknown stretch directive".into()),
+                        }
+                    }
+                }
+                (Some(":"), _) => Err("missing directive".into()),
+                (Some(s), _) => Err(format!("unknown directive {}", s)),
+                (None, _) => Err("missing time argument".into()),
+            }
+        } else {
+            match s.parse::<u8>() {
+                Ok(minutes) => Ok(Self::Minutes(minutes)),
+                Err(ctx) => Err(format!("{}", ctx)),
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -20,9 +59,8 @@ enum DateLog {
 pub struct BookingArgs {
     /// Project alias
     alias: String,
-    /// Time in minutes or a stretch pattern (e.g. <int> | h:<int> | ::HH:MM | ::last)
-    #[structopt(parse(try_from_str = parse_time))]
-    time: TimeLog,
+    /// Time in minutes or a stretch pattern (e.g. <int> | h::<f64> | <s or t>::HH:MM | s::last)
+    time: TimeArg,
     /// Date in isoformat or weekday (e.g. "YYYY-MM-DD" | <weekday>)
     #[structopt(short="d", long="date", parse(try_from_str = parse_date))]
     date: Option<DateLog>,
@@ -32,38 +70,6 @@ pub struct BookingArgs {
     ticket: Option<String>,
     /// Reference to git branch for work (e.g. "feature/RAS-002")
     branch: Option<String>,
-}
-
-fn parse_time(input: &str) -> Result<TimeLog, String> {
-    match input.to_owned().parse::<u8>() {
-        Ok(minutes) => Ok(TimeLog::Minutes(minutes)),
-        Err(_) => match (input.get(..2), input.get(2..)) {
-            // TODO: Implement reading latest log time
-            (Some("::"), Some("last")) => Ok(TimeLog::Stretch(Time::now(), Time::now())),
-            (Some("::"), Some(time)) => match Time::parse(format!("{}:00", time), "%T") {
-                Ok(t) => Ok(TimeLog::Stretch(t, Time::now())),
-                Err(ctx) => Err(format!(
-                    "{} is an invalid stretch pattern. The correct format is HH:MM",
-                    ctx
-                )),
-            },
-            (Some("::"), None) => {
-                Err("Stretch pattern requires an argument (last or HH:MM)".into())
-            }
-            (Some("h:"), Some(hours)) => match hours.parse::<u8>() {
-                Ok(h) => Ok(TimeLog::Hours(h)),
-                Err(ctx) => Err(format!(
-                    "{} for hours. The correct format is 'h:<int>'",
-                    ctx
-                )),
-            },
-            (Some("h:"), None) => Err("Hour pattern requires an argument (<int>)".into()),
-            (Some(head), Some(tail)) => {
-                Err(format!("Unable to parse time value: {}{}", head, tail))
-            }
-            (_, _) => Err("No value was given".into()),
-        },
-    }
 }
 
 fn parse_date(input: &str) -> Result<DateLog, String> {
@@ -88,32 +94,18 @@ mod tests {
 
     #[test]
     fn time_parser_ok() {
-        assert!(
-            parse_time("::last").is_ok(),
-            "Fail on parsing last time stretch"
-        );
-        assert!(
-            parse_time("::08:00").is_ok(),
-            "Fail on parsing defined time stretch"
-        );
-        assert_eq!(
-            parse_time("h:8"),
-            Ok(TimeLog::Hours(8)),
-            "Fail on parsing hours"
-        );
-        assert_eq!(
-            parse_time("60"),
-            Ok(TimeLog::Minutes(60)),
-            "Fail on parsing minutes"
-        );
+        assert!(TimeArg::from_str("s::last").is_ok());
+        assert!(TimeArg::from_str("s::08:00").is_ok());
+        assert!(TimeArg::from_str("h::1.5").is_ok());
+        assert!(TimeArg::from_str("60").is_ok());
     }
     #[test]
     fn time_parser_err() {
-        assert!(parse_time("::lasts").is_err());
-        assert!(parse_time("::08").is_err());
-        assert!(parse_time("::08:00a").is_err());
-        assert!(parse_time("").is_err());
-        assert!(parse_time("abc").is_err());
+        assert!(TimeArg::from_str("::lasts").is_err());
+        assert!(TimeArg::from_str("::08").is_err());
+        assert!(TimeArg::from_str("::08:00a").is_err());
+        assert!(TimeArg::from_str("").is_err());
+        assert!(TimeArg::from_str("abc").is_err());
     }
 
     #[test]
