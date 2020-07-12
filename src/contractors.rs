0cebf640cf8319fdace85e4d1e9a152fb916c756
fliepeltje <donatas@rasiukevicius.dev>
2020-07-12 23:38:21 +0200

    Implement filter for contractor

--- src/contractors.rs
+++ src/contractors.rs
@@ -1,9 +1,9 @@
 use crate::errors::CliError;
 use crate::generics::{
-    add_subject, delete_subject, update_subject, view_subject, Crud, Result, View,
+    add_subject, delete_subject, update_subject, view_filtered_set, view_subject, Crud, Filter,
+    Result, View,
 };
 use crate::utils::slugify;
-use crate::Action;
 use colored::*;
 use read_input::prelude::*;
 use serde::{Deserialize, Serialize};
@@ -12,29 +12,51 @@ use std::str::FromStr;
 use structopt::StructOpt;
 use toml::{from_str as from_toml, to_string as to_toml};
 
-pub fn exec_cmd_contractor(args: ContractorArgs) -> Result<()> {
-    match (args.action, args.slug) {
-        (Action::Add, None) => add_subject(Contractor::new())?,
-        (Action::Delete, Some(slug)) => delete_subject::<Contractor>(&slug)?,
-        (Action::Update, Some(slug)) => update_subject::<Contractor>(&slug)?,
-        (Action::View, maybe_slug) => view_subject::<Contractor>(maybe_slug)?,
-        (Action::Add, Some(_)) => println!("The add action does not require a slug"),
-        (action, None) => println!("{} requires a slug", action.to_string().bold()),
-    };
-    Ok(())
-}
-
-#[derive(StructOpt, Debug)]
-pub struct ContractorArgs {
-    /// Type of operation to perform
-    action: Action,
-    slug: Option<String>,
-}
-
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Contractor {
     pub slug: String,
     pub name: String,
+}
+
+#[derive(StructOpt, Debug)]
+pub enum Cmd {
+    /// Create a new contractor interactively
+    #[structopt(name = "add")]
+    Create,
+    /// Update an existing contractor interactively
+    #[structopt(name = "update")]
+    Update { contractor: Contractor },
+    /// View a collection of contractors
+    #[structopt(name = "show")]
+    Show {
+        #[structopt(short = "f")]
+        filters: Vec<F>,
+        #[structopt(short = "s", default_value = "no_sort")]
+        sort: S,
+    },
+    /// View detailed contractor stats
+    #[structopt(name = "detail")]
+    Detail { contractor: Contractor },
+    /// Delete a contractor
+    #[structopt(name = "delete")]
+    Delete { contractor: Contractor },
+}
+
+impl Cmd {
+    pub fn exec(&self) -> Result<()> {
+        match self {
+            Self::Create => add_subject(Contractor::new()?)?,
+            Self::Delete { contractor } => delete_subject::<Contractor>(&contractor.slug)?,
+            Self::Update { contractor } => update_subject::<Contractor>(&contractor.slug)?,
+            Self::Detail { contractor } => {
+                view_subject::<Contractor>(Some(contractor.slug.clone()))?
+            }
+            Self::Show { filters, sort } => {
+                view_filtered_set::<Contractor, F, S>(filters.to_vec(), sort.clone())?
+            }
+        };
+        Ok(())
+    }
 }
 
 impl Crud<'_> for Contractor {
@@ -81,7 +103,7 @@ impl FromStr for Contractor {
 }
 
 impl Contractor {
-    fn new() -> Self {
+    fn new() -> Result<Self> {
         let name = input::<String>().msg("Contractor name: ").get();
         let slug = slugify(name.clone());
         let slug_msg = format!(
@@ -93,6 +115,50 @@ impl Contractor {
             .msg(slug_msg)
             .default(slug)
             .get();
-        Self { slug, name }
+        Ok(Self { slug, name })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum F {
+    NoFilter,
+}
+
+impl FromStr for F {
+    type Err = CliError;
+
+    fn from_str(input: &str) -> Result<Self> {
+        Ok(F::NoFilter)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum S {
+    NoSort,
+}
+
+impl FromStr for S {
+    type Err = CliError;
+
+    fn from_str(input: &str) -> Result<Self> {
+        Ok(Self::NoSort)
+    }
+}
+
+impl Filter<F, S> for Contractor {
+    const DEFAULT_SORT: S = S::NoSort;
+    const DEFAULT_FILTER: F = F::NoFilter;
+
+    fn get_base_items() -> Result<Vec<Self>> {
+        let mapping = Self::mapping()?;
+        Ok(mapping.values().cloned().collect::<Vec<Self>>())
+    }
+
+    fn filter(items: Vec<Self>, method: F) -> Vec<Self> {
+        items
+    }
+
+    fn sort(items: Vec<Self>, method: S) -> Vec<Self> {
+        items
     }
 }
